e690c26d29cfedaaccd368401f8c19399058a1bf
jean <jeang51@nycstudents.net>
2025-04-02 09:21:00 -0400

    ggggggggg

--- teachers.py
+++ teachers.py
@@ -1,22 +1,15 @@
-class FlashCards:
-    def __init__(self, Questions, answers): 
-        self.question = Questions
-        self.answer = answers
-        
-        
-    
-    def ask_question(self):
-        print(self.question)
-        
-    
-    def ask_question(self):
-        print(self.answer)
-         
-        question = input("enter a question:")
-        answer = input("enter the answer:")
-        
-        FlashCards
+class teacher:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
+    def display_info(self):
+        return f"User: {self.name}, Email: {self.email}"
+
+        
+    
+         
+       
 
        
         
