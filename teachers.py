bbda6084abdbf70ceeaa18381a734b05a2801fbb
jean <jeang51@nycstudents.net>
2025-04-08 09:21:59 -0400

    2rbb

--- teachers.py
+++ teachers.py
@@ -1,18 +1,17 @@
-Q = {}
-p = []
+import json
 
+try:
+    with open("FlashCard.json", "r") as file:
+        FlashCard_data = json.load(file)
+except FileNotFoundError:
+    FlashCard_data = {}
 
-
-class teacher:
-    def ask_question(self):
-        print(self.answer)
-         
-        question = input("enter a question:")
-        answer = input("enter the answer:")
-        
-    def display_info(self): 
-         return f"teacher: {self.question}, Email: {self.answer}"
-
+# Define the FlashCard class
+class FlashCard:
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+    
              
     
         
