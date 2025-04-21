f45744475c6883a2887392263936b579ae1274d1
jean <jeang51@nycstudents.net>
2025-04-21 09:22:50 -0400

    rrgwerhgrfgdfgdfgfg

--- teachers.py
+++ teachers.py
@@ -14,13 +14,12 @@ class FlashCard:
         self.answer = answer
 
 # Function to add a new flashcard
+    def to_dict(self):
+        return {self.question: self.answer}
+    
+    
 
 
-def add_flashcard(question, answer):
-    q = {
-        "Question":question,
-        "Answer":answer
-    }
     # Write the updated dictionary back to the JSON file
     with open("FlashCard.json", "w") as file:
         json.dump(q, file, indent=4)
